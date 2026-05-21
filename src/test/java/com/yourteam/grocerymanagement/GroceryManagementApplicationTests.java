68dccfa7dc7e3186c49fe1ab233efa10da3bf632
IT25101538 <it25101538@my.sliit.lk>
2026-05-21 23:27:08 +0530

    Version5- BhagyaIdumini

---
+++ src/test/java/com/yourteam/grocerymanagement/GroceryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourteam.grocerymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
